652d5778980c851a19b8c6a28ab22f413e7d227d
Chris Ingram <chris@chrisingram.me>
2016-12-26 18:49:05 +0000

    added projects to homepage

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,9 @@ const gulp = require('gulp');
 const handlebars = require('gulp-compile-handlebars');
 const rename = require('gulp-rename');
 
+const watch = require("gulp-watch");
+const batch = require("gulp-batch");
+
 gulp.task('copy', function() {
   gulp.src('assets/**')
     .pipe(gulp.dest('./docs/assets'));
@@ -18,6 +21,15 @@ gulp.task('html', () => {
       extname: '.html'
     }))
     .pipe(gulp.dest('./docs'));
+});
+
+gulp.task('watch', function () {
+  watch('src/**', batch(function (events, done) {
+      gulp.start('html', done);
+  }));
+  watch('assets/**', batch(function (events, done) {
+      gulp.start('copy', done);
+  }));
 });
 
 gulp.task('default', ['copy', 'html']);
